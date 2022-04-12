34bfdc4e70fc928f7012bfb7e2fabcf1edb0ed21
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2022-04-12 22:44:59 +0900

    data 가져오는 로직

---
+++ src/shared/data.js
@@ -0,0 +1,14 @@
+class Data {
+  async getTitle(value, setRelatedList) {
+    const result = await fetch(
+      `http://hn.algolia.com/api/v1/search?query=${value}`,
+    ); //
+    const response = await result.json();
+    const data = response.hits;
+
+    console.log(data);
+    setRelatedList(data);
+  }
+}
+
+export default Data;
