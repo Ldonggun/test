bb6082c6d0281f0206068b95394022f0d5224079
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2022-04-12 22:45:41 +0900

    debounce func

---
+++ src/shared/debounce.js
@@ -0,0 +1,9 @@
+export default function debounce(callback, delay) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback.apply(this, args);
+    }, delay);
+  };
+}
