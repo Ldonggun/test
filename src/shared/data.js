f368ad4ece8e4ebd3be093e1e712d9b9617de3dd
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2022-04-16 14:28:17 +0900

    api 추가

--- src/shared/data.js
+++ src/shared/data.js
@@ -8,6 +8,13 @@ class Data {
 
     setRelatedList(data);
   }
+
+  async getComment(id, setData) {
+    const result = await fetch(`http://hn.algolia.com/api/v1/items/${id}`);
+    const response = await result.json();
+
+    setData(response);
+  }
 }
 
 export default Data;
